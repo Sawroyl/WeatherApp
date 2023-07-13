db6caf62f415360e58134b1f9dff0e13c5bd9fb0
Sawroyl <127824275+Sawroyl@users.noreply.github.com>
2023-07-13 18:53:21 +0545

    Add files via upload
    Weather app prototype-1 mid progress

---
+++ Weather/main.js
@@ -0,0 +1,57 @@
+const apiKey = "5cb1e7c1958db1327fd255ff75b6264b";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const defaultLocation = "Bedworth"
+
+const searchBox = document.querySelector(".search input");
+const searchButton = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon"); 
+async function checkWeather(city){
+    const response =  await fetch(apiUrl + city + `&appid=${apiKey}`);
+    var data = await response.json();
+    
+    if (data.cod == "400"){
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+    }
+    else{
+        
+    console.log(data);
+    
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temp").innerHTML = data.main.temp +" °C";
+        document.querySelector(".humidity").innerHTML = data.main.humidity +" %";
+        document.querySelector(".wind").innerHTML = data.wind.speed +" Km/H";           
+        
+            if(data.weather[0].main == "Clouds"){
+                weatherIcon.src = "images/cloud.png"
+            }
+            else if(data.weather[0].main == "Clear"){
+                weatherIcon.src = "images/cloud.png"
+            }
+            else if(data.weather[0].main == "Rain"){
+                weatherIcon.src = "images/cloud.png"
+            }
+            else if(data.weather[0].main == "Drizzle"){
+                weatherIcon.src = "images/cloud.png"
+            }
+            else if(data.weather[0].main == "Snow"){
+                weatherIcon.src = "images/cloud.png"
+            }
+            else if(data.weather[0].main == "Thunderstorm"){
+                weatherIcon.src = "images/cloud.png"
+            }
+            else if(data.weather[0].main == "Atmosphere"){
+                weatherIcon.src = "images/cloud.png"
+            }
+            document.querySelector(".error").style.display = "none";
+            document.querySelector(".weather").style.display = "block";
+        }
+    
+}   
+checkWeather(defaultLocation)
+searchButton.addEventListener("click", ()=>{
+    checkWeather(searchBox.value)
+})
+
+
