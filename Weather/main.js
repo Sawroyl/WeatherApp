06639ea8600aba5b50fcda6ef4d4076b9c7b2ffd
Sawroyl <127824275+Sawroyl@users.noreply.github.com>
2023-07-16 18:38:00 +0545

    finalll

--- Weather/main.js
+++ Weather/main.js
@@ -2,55 +2,56 @@ const apiKey = "5cb1e7c1958db1327fd255ff75b6264b";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
 const defaultLocation = "Bedworth"
-
+d = new Date()
+document.getElementById("fullDate").innerHTML = d;
 const searchBox = document.querySelector(".search input");
 const searchButton = document.querySelector(".search button");
-const weatherIcon = document.querySelector(".weather-icon"); 
-async function checkWeather(city){
-    const response =  await fetch(apiUrl + city + `&appid=${apiKey}`);
+const weatherIcon = document.querySelector(".weather-icon");
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
     var data = await response.json();
-    
-    if (data.cod == "400"){
+
+    if (data.cod == "400") {
         document.querySelector(".error").style.display = "block";
         document.querySelector(".weather").style.display = "none";
     }
-    else{
-        
-    console.log(data);
-    
-        document.querySelector(".city").innerHTML = data.name;
-        document.querySelector(".temp").innerHTML = data.main.temp +" °C";
-        document.querySelector(".humidity").innerHTML = data.main.humidity +" %";
-        document.querySelector(".wind").innerHTML = data.wind.speed +" Km/H";           
-        
-            if(data.weather[0].main == "Clouds"){
-                weatherIcon.src = "images/cloud.png"
-            }
-            else if(data.weather[0].main == "Clear"){
-                weatherIcon.src = "images/cloud.png"
-            }
-            else if(data.weather[0].main == "Rain"){
-                weatherIcon.src = "images/cloud.png"
-            }
-            else if(data.weather[0].main == "Drizzle"){
-                weatherIcon.src = "images/cloud.png"
-            }
-            else if(data.weather[0].main == "Snow"){
-                weatherIcon.src = "images/cloud.png"
-            }
-            else if(data.weather[0].main == "Thunderstorm"){
-                weatherIcon.src = "images/cloud.png"
-            }
-            else if(data.weather[0].main == "Atmosphere"){
-                weatherIcon.src = "images/cloud.png"
-            }
-            document.querySelector(".error").style.display = "none";
-            document.querySelector(".weather").style.display = "block";
+    else {
+
+        console.log(data);
+
+        document.querySelector(".city").innerHTML = "City" + ": " + data.name;
+        document.querySelector(".temp").innerHTML = data.main.temp + " °C";
+        document.querySelector(".humidity").innerHTML = data.main.humidity + " %";
+        document.querySelector(".wind").innerHTML = data.wind.speed + " Km/H";
+
+        if (data.weather[0].main == "Clouds") {
+            weatherIcon.src = "img/cloudy.png"
         }
-    
-}   
+        else if (data.weather[0].main == "Clear") {
+            weatherIcon.src = "img/cloud.png"
+        }
+        else if (data.weather[0].main == "Rain") {
+            weatherIcon.src = "img/cloud.png"
+        }
+        else if (data.weather[0].main == "Drizzle") {
+            weatherIcon.src = "img/cloud.png"
+        }
+        else if (data.weather[0].main == "Snow") {
+            weatherIcon.src = "img/cloud.png"
+        }
+        else if (data.weather[0].main == "Thunderstorm") {
+            weatherIcon.src = "img/cloud.png"
+        }
+        else if (data.weather[0].main == "Atmosphere") {
+            weatherIcon.src = "img/cloud.png"
+        }
+        document.querySelector(".error").style.display = "none";
+        // document.querySelector(".weather").style.display = "block";
+    }
+
+}
 checkWeather(defaultLocation)
-searchButton.addEventListener("click", ()=>{
+searchButton.addEventListener("click", () => {
     checkWeather(searchBox.value)
 })
 
